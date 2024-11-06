fc49935441b6278ed1aa5ef6e23d955e3bcb8d24
the13thgeek <aldreich.aguilar@yahoo.com>
2024-11-06 17:38:03 -0500

    Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queue, setQueue] = useState([
+    {
+        "title": "Xanadu",
+        "artist": "The Olivia Project",
+        "username": "the13thgeek"
+    },
+    {
+        "title": "Let Them Move",
+        "artist": "N.M.R.",
+        "username": "the13thgeek"
+    }
+  ]);
+
+  const addSong = (newSong) => {
+    setQueue([...queue, newSong]);
+  }
+
+  const testAddSong = () => {
+    setQueue([...queue, { "title": "NEW", "artist": "NEW", "username": "@the13thgeek" }]);
+  }
+
+  const removeFirstSong = () => {
+    setQueue((prevQueue) => prevQueue.slice(1));
+  }
 
   return (
-    <>
-      <p>Hello World!</p>
-    </>
+    <div>
+        <ul className="queue">
+            <AnimatePresence>
+                {queue.map((song, index) => (
+                    <motion.li
+                        key={index}
+                        initial={ {opacity: 0, x: 100} }
+                        animate={ {opacity: 1, x: 0} }
+                        exit={ {opacity: 0, x: -100} }
+                        transition={ {duration: 0.3}}
+                        className='song-item'
+                        ><b>{song.title}</b><br />
+                        {song.artist}<br />
+                        <small>{song.username}</small>
+                    </motion.li>
+                ))}
+            </AnimatePresence>
+        </ul>
+
+        {/* <div className="testbuttons">
+            <button onClick={() => testAddSong()}>Test Add Song</button>
+            <button onClick={() => removeFirstSong()}>Test Remove Song</button>
+        </div> */}
+    </div>
   )
 }
 
